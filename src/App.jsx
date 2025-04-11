e4485c1968d63a26a6ada06f66adcc87ce45cf1a
NeiltonSeguins <seguins.neilton@gmail.com>
2025-04-11 11:46:00 -0300

    Consome a API e aplica boas práticas de segurança em frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
+import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import ForecastList from "./components/Forecast/ForecastList";
 import "./App.css";
 
-const forecasts = [
-  { day: "Amanhã", date: "02/12", min: 20, max: 34 },
-  { day: "Quarta-feira", date: "03/12", min: 20, max: 34 },
-  { day: "Quinta-feira", date: "04/12", min: 20, max: 34 },
-]
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=São Luís,MA`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results) {
+          setWeatherData(data.results);
+        } else {
+          console.error("Erro na resposta da API:", data);
+        }
+      })
+      .catch((error) => console.error("Erro na requisição:", error));
+  }, []);
+
   return (
     <div className="app-container">
       <SearchBar />
-      <h1>Florianópolis - SC</h1>
-      <WeatherCard
-        temperature={33}
-        condition="Sol entre nuvens"
-        humidity={93}
-        minTemp={20}
-        maxTemp={34}
-      />
-      <ForecastList
-        forecasts={forecasts}
-      />
+      {weatherData && (
+        <>
+          <h1>{weatherData.city}</h1>
+          <WeatherCard
+            temperature={weatherData.temp}
+            condition={weatherData.description}
+            humidity={weatherData.humidity}
+            minTemp={weatherData.forecast[0].min}
+            maxTemp={weatherData.forecast[0].max}
+          />
+          <ForecastList forecasts={weatherData.forecast.slice(1, 4)} />
+        </>
+      )}
     </div>
   );
 }
