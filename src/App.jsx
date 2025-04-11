a0100c6b57e70987128a1c5dfff729c61c610f60
NeiltonSeguins <seguins.neilton@gmail.com>
2025-04-11 16:18:07 -0300

    Adiciona um componente e estado de loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import ForecastList from "./components/Forecast/ForecastList";
+import Loading from "./components/Loading";
 import "./App.css";
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
@@ -10,9 +11,11 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
   const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchWeather() {
+      setLoading(true);
       try {
         const response = await fetch(
           `https://api.hgbrasil.com/weather?format=json-cors&key=${API_KEY}&city_name=${city}`
@@ -25,6 +28,8 @@ function App() {
         }
       } catch (error) {
         console.error("Erro ao buscar dados da API", error);
+      } finally {
+        setLoading(false);
       }
     }
 
@@ -34,12 +39,16 @@ function App() {
   return (
     <div className="app-container">
       <SearchBar onSearch={setCity} />
-      {weather && (
+      {loading ? (
+        <Loading />
+      ) : weather ? (
         <>
           <h1>{weather.city}</h1>
           <WeatherCard weather={weather} />
           <ForecastList forecasts={forecast} />
         </>
+      ) : (
+        <p>Digite uma cidade para buscar o clima.</p>
       )}
     </div>
   );
